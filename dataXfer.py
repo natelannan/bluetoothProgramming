e999e5d8705890f56583d3a52c816dd30c3e4ada
natelannan <natelannan@gmail.com>
2016-04-04 12:52:41 -0500

    added try catch for timeout when trying to send.  need to catch more specific error than bluetooth error.

--- dataXfer.py
+++ dataXfer.py
@@ -3,6 +3,7 @@ import re
 import bluetooth
 import select
 import time
+import socket
 
 LMm_addr = "38:C0:96:41:0E:A2"
 uuid = "00001101-0000-1000-8000-00805f9b34fb"
@@ -20,9 +21,10 @@ print "Connecting to \"%s\" on %s on port %s" % (name, host, port)
 
 #sock=bluetooth.BluetoothSocket(bluetooth.SCO)
 sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-#sock.setblocking(0)
+
 #port=bluetooth.get_available_port(bluetooth.RFCOMM)
 sock.connect((host,port))
+sock.settimeout(0.5)
 #sock.connect((host))
 #sock.bind((LMm_addr,port))
 #sock.listen(1)
@@ -35,7 +37,8 @@ getData="\002ETC:FWD ?;\003\032"
 ack="\006"
 
 while 1:
-    time.sleep(.5)
+    #time.sleep(.5)
+    
     sock.send(getData)
     
     field=0
@@ -46,24 +49,29 @@ while 1:
     
     while '\003' not in data:
         #ready=select.select([sock],[],[],2)
+        
         #if ready[0]:
-        character=sock.recv(1)
-        #print character
-        data=data+character
-        if data == '\006\002ETC:FWD ':
-            #print 'foo'
-            field=1
-            #print "received [%r]" % data[i]
-        if character == ";":
-            field=field+1
-        elif field==5:
-            #print 'bar'
-            meas=meas+character
-        elif field==1:
-            #print 'baz'
-            intensity=intensity+character
-        elif field==4:
-            error =int(character)
+        try:
+            character=sock.recv(1)
+            #print character
+            data=data+character
+            if data == '\006\002ETC:FWD ':
+                #print 'foo'
+                field=1
+                #print "received [%r]" % data[i]
+            if character == ";":
+                field=field+1
+            elif field==5:
+                #print 'bar'
+                meas=meas+character
+            elif field==1:
+                #print 'baz'
+                intensity=intensity+character
+            elif field==4:
+                error =int(character)
+        except bluetooth.btcommon.BluetoothError:  #bluetooth.btcommon.BluetoothError
+            print "Resending"
+            sock.send(getData)
         #else:
             #print "Resending"
             #sock.send(getData)
