bd2923e0c94f410acb1f93792218422863190495
natelannan <natelannan@gmail.com>
2016-09-23 11:13:03 -0500

    simple discover of desired blue tooth address

---
+++ discover.py
@@ -0,0 +1,17 @@
+import bluetooth
+
+target_name = "LMm-G"
+target_address = None
+nearby_devices = bluetooth.discover_devices()
+
+#bluetooth detection and name lookup are probablilistic
+#if this fails try a couple more times
+for bdaddr in nearby_devices:
+    if target_name == bluetooth.lookup_name(bdaddr):
+        target_address = bdaddr
+        break
+
+if target_address is not None:
+    print "found target bluetooth device with address ", target_address
+else:
+    print "could not find target bluetooth device nearby"
